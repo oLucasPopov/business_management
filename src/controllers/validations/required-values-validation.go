115a2d2fb7d4a31539dbddb3ea29ba59a53b0d5c
oLucasPopov <lucaspopov@protonmail.com>
2023-10-12 10:06:23 -0300

    feat: validates required values in fields

---
+++ src/controllers/validations/required-values-validation.go
@@ -0,0 +1,32 @@
+package validations
+
+import (
+	"encoding/json"
+	"fmt"
+	controller_protocols "pontos_funcionario/src/controllers/protocols"
+	"slices"
+)
+
+type RequiredValuesValidation struct {
+	validValues []interface{}
+	fieldName   string
+	controller_protocols.Validation
+}
+
+func NewRequiredValuesValidation(field string, valuesToValidate []interface{}) *RequiredValuesValidation {
+	return &RequiredValuesValidation{
+		validValues: valuesToValidate,
+		fieldName:   field,
+	}
+}
+
+func (c *RequiredValuesValidation) Validate(input string) error {
+	mapInput := map[string]interface{}{}
+	_ = json.Unmarshal([]byte(input), &mapInput)
+
+	if !slices.Contains(c.validValues, mapInput[c.fieldName]) {
+		return fmt.Errorf("the value provided for the field %s is invalid", c.fieldName)
+	}
+
+	return nil
+}
