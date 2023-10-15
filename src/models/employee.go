b8d26ba10fa5ade6b1881d9f099f61df4d0eecf1
oLucasPopov <lucaspopov@protonmail.com>
2023-10-15 09:38:28 -0300

    refactor: make employee json return all fields

--- src/models/employee.go
+++ src/models/employee.go
@@ -9,18 +9,18 @@ const (
 
 type AddEmployee struct {
 	Name          *string     `json:"name"`
-	CPF_CNPJ      *string     `json:"cpf_cnpj,omitempty"`
-	Phone         *string     `json:"phone,omitempty"`
-	Cellphone     *string     `json:"cellphone,omitempty"`
-	Email         *string     `json:"email,omitempty"`
-	Obs           *string     `json:"obs,omitempty"`
-	State_id      *int64      `json:"state_id,omitempty"`
-	Town_id       *int64      `json:"town_id,omitempty"`
-	Zipcode       *string     `json:"zipcode,omitempty"`
-	Address       *string     `json:"address,omitempty"`
-	Number        *string     `json:"number,omitempty"`
-	Neighbourhood *string     `json:"neighbourhood,omitempty"`
-	Reference     *string     `json:"reference,omitempty"`
+	CPF_CNPJ      *string     `json:"cpf_cnpj"`
+	Phone         *string     `json:"phone"`
+	Cellphone     *string     `json:"cellphone"`
+	Email         *string     `json:"email"`
+	Obs           *string     `json:"obs"`
+	State_id      *int64      `json:"state_id"`
+	Town_id       *int64      `json:"town_id"`
+	Zipcode       *string     `json:"zipcode"`
+	Address       *string     `json:"address"`
+	Number        *string     `json:"number"`
+	Neighbourhood *string     `json:"neighbourhood"`
+	Reference     *string     `json:"reference"`
 	SalaryType    *SalaryType `json:"salary_type"`
 	Salary        *float32    `json:"salary"`
 }
