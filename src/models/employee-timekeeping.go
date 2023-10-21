05bcd692793a95aab073bec794f8e57adbb029d5
oLucasPopov <lucaspopov@protonmail.com>
2023-10-21 13:00:43 -0300

    feat: created timekeeping models

---
+++ src/models/employee-timekeeping.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+type AddClockInEmployee struct {
+	EmployeeId      *int64     `json:"employee_id"`
+	ClockInDateTime *time.Time `json:"clock_in"`
+	EmployeeSalary  *float32   `json:"salary"`
+}
+
+type TimeKeepingEmployee struct {
+	Id               *int64     `json:"id"`
+	ClockOutDateTime *time.Time `json:"clock_out"`
+	AddClockInEmployee
+}
