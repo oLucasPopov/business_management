beaa5f9cab8fc88278a4eb46bdb2836489882920
oLucasPopov <lucaspopov@protonmail.com>
2023-10-31 19:45:59 -0300

    refactor: start server into a function

--- main.go
+++ main.go
@@ -7,8 +7,24 @@ import (
 	"pontos_funcionario/src/config"
 	app_routes "pontos_funcionario/src/main/routes"
 
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
+
+func startServer(r *mux.Router) (string, *mux.Router) {
+	hostConfig, err := config.GetHostConfig()
+
+	if err != nil {
+		log.Panic(err)
+	} else {
+		fmt.Println("server configs loaded")
+	}
+
+	serverString := fmt.Sprintf("%s:%d", hostConfig.Host, hostConfig.Port)
+	fmt.Println("Starting server at", serverString)
+
+	return serverString, r
+}
 
 func main() {
 	err := godotenv.Load()
@@ -18,13 +34,6 @@ func main() {
 		fmt.Println("database configs loaded")
 	}
 
-	hostConfig, err := config.GetHostConfig()
-	if err != nil {
-		log.Panic(err)
-	} else {
-		fmt.Println("server configs loaded")
-	}
-
 	r := app_routes.MakeRoutes()
-	http.ListenAndServe(fmt.Sprintf("%s:%d", hostConfig.Host, hostConfig.Port), r)
+	http.ListenAndServe(startServer(r))
 }
